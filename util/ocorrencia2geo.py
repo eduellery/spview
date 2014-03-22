9d6e6e27f899ac62dbdd24aaee8667fbcbe27372
eltermann <felipe.eltermann@gmail.com>
2014-03-22 18:51:02 -0300

    Added ocorrencia2geo.py

---
+++ util/ocorrencia2geo.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 22 18:37:04 2014
+
+@author: felipe
+"""
+
+from pygeocoder import Geocoder
+from pygeolib import GeocoderError
+import psycopg2
+
+"""
+ALTER TABLE ocorrencia ADD lat text;
+ALTER TABLE ocorrencia ADD lon text;
+"""
+
+# ssh tunneled: ssh eltermann@143.106.60.97 -L 54321:localhost:5432
+conn = psycopg2.connect("dbname='spview' user='postgres' host='localhost' port='5432' password='Zem5aEpEH6dB5aF'");
+cur = conn.cursor()
+
+geocoder = Geocoder()
+
+cur.execute("""SELECT ocorrencia, local, altura FROM ocorrencia""")
+result = cur.fetchall()
+for row in result:
+  cod_ocorrencia = row[0]
+  rua = row[1]
+  altura = row[2]
+  if altura and altura != 'NULL':
+    address = '%s, %s' % (rua, altura)
+  else:
+    address = rua
+
+  try:
+    ret = geocoder.geocode(address)
+    lat, lon = ret.coordinates
+  except GeocoderError as e:
+    lat, lon = ('', '')
+
+  cur.execute("""UPDATE ocorrencia SET lat=%s, lon=%s WHERE ocorrencia=%s""", (lat, lon, cod_ocorrencia))
+conn.commit()
