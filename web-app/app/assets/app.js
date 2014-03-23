c0212c790eb4924e16a52db10322d7582594391f
Oscar Esgalha <oscaresgalha@gmail.com>
2014-03-22 23:45:46 -0300

    Esboco do torque

--- web-app/app/assets/app.js
+++ web-app/app/assets/app.js
@@ -6,71 +6,67 @@ $('#camadas-button').click(function() {
   $('#camadas-menu').sidebar('toggle');
 });
 
-var mapOptions = {
-  zoom: 10,
-  center: new google.maps.LatLng(-23.546523, -46.633407)
-};
-
-var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-var sublayers = [];
-
-cartodb.createLayer(map, 'http://oesgalha.cartodb.com/api/v2/viz/4a933b04-b1bf-11e3-8f25-0e230854a1cb/viz.json')
-.addTo(map)
-.on('done', function(layer) {
-  // change the query for the first layer
-  var subLayerOptions = {
-    sql: "SELECT * FROM table_2013_acidentes_1o_semestre WHERE vit_morta = 0",
-    cartocss: "#table_2013_acidentes_1o_semestre{marker-opacity: 0.9;marker-line-color: #FFF;marker-line-width: 1.5;marker-line-opacity: 1;marker-placement: point;marker-multi-policy: largest;marker-type: ellipse;marker-fill: #FF5C00;marker-allow-overlap: true;marker-clip: false;}#table_2013_acidentes_1o_semestre [ vit_ferida <= 23] {marker-width: 25.0;}#table_2013_acidentes_1o_semestre [ vit_ferida <= 11] {marker-width: 23.3;}#table_2013_acidentes_1o_semestre [ vit_ferida <= 9] {marker-width: 21.7;}#table_2013_acidentes_1o_semestre [ vit_ferida <= 8] {marker-width: 20.0;}#table_2013_acidentes_1o_semestre [ vit_ferida <= 7] {marker-width: 18.3;}#table_2013_acidentes_1o_semestre [ vit_ferida <= 6] {marker-width: 16.7;}#table_2013_acidentes_1o_semestre [ vit_ferida <= 4] {marker-width: 15.0;}#table_2013_acidentes_1o_semestre [ vit_ferida <= 3] {marker-width: 13.3;}#table_2013_acidentes_1o_semestre [ vit_ferida <= 2] {marker-width: 11.7;}#table_2013_acidentes_1o_semestre [ vit_ferida <= 1] {marker-width: 10.0;}"
-  }
-
-  var sublayer = layer.getSubLayer(0);
-
-  sublayer.set(subLayerOptions);
-
-  sublayers.push(sublayer);
-
- subLayerOptions = {
-    sql: "SELECT * FROM table_2013_acidentes_1o_semestre WHERE vit_morta >= 1",
-    cartocss: "#table_2013_acidentes_1o_semestre{marker-opacity: 0.9;marker-line-color: #FFF;marker-line-width: 1.5;marker-line-opacity: 1;marker-placement: point;marker-multi-policy: largest;marker-type: ellipse;marker-fill: #B40903;marker-allow-overlap: true;marker-clip: false;}#table_2013_acidentes_1o_semestre [ vit_morta <= 3] {marker-width: 25.0;}#table_2013_acidentes_1o_semestre [ vit_morta <= 3] {marker-width: 23.3;}#table_2013_acidentes_1o_semestre [ vit_morta <= 2] {marker-width: 21.7;}#table_2013_acidentes_1o_semestre [ vit_morta <= 2] {marker-width: 20.0;}#table_2013_acidentes_1o_semestre [ vit_morta <= 2] {marker-width: 18.3;}#table_2013_acidentes_1o_semestre [ vit_morta <= 2] {marker-width: 16.7;}#table_2013_acidentes_1o_semestre [ vit_morta <= 1] {marker-width: 15.0;}#table_2013_acidentes_1o_semestre [ vit_morta <= 1] {marker-width: 13.3;}#table_2013_acidentes_1o_semestre [ vit_morta <= 1] {marker-width: 11.7;}#table_2013_acidentes_1o_semestre [ vit_morta <= 0] {marker-width: 10.0;}"
-  }
-
-  sublayer = layer.getSubLayer(1);
-
-  sublayer.set(subLayerOptions);
-
-  sublayers.push(sublayer);
-
-}).on('error', function() {
-  //log the error
+var map = new google.maps.Map(document.getElementById('map'), {
+    center: new google.maps.LatLng(-23.546523, -46.633407),
+    zoom: 2,
+    mapTypeId:google.maps.MapTypeId.SATELLITE,
+    mapTypeControl:false,
+    minZoom:1,
+    scaleControl: false,
+    streetViewControl: false,
+    overviewMapControl: false,
 });
 
-$('.camadas-control').change(function() {
-  if ($(this).is(':checked')) {
-    LayersShowAction[$(this).attr('name')]();
-  } else {
-    LayersHideAction[$(this).attr('name')]();
-  }
+var map_style = {};
+map_style.google_maps_customization_style = [
+    {
+        stylers:[
+            { invert_lightness: true },
+            { weight: 1 },
+            { saturation: -100 },
+            { lightness: -40 }
+        ]
+    },
+    {
+        elementType:"labels",
+        stylers:[
+            { visibility:"simplified" }
+        ]
+    }
+];
+
+map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+map.setOptions({styles:map_style.google_maps_customization_style});
+
+var torqueLayer = new torque.GMapsTorqueLayer({
+  provider: 'sql_api',
+  user : 'ellery',
+  table : 'lentidao_sumario_semanal3',
+  column : 'cartodb_id',
+  countby : 'count(extensao)',
+  resolution: 1,
+  steps: 1024,
+  blendmode : 'lighter',
+  animationDuration: 60,
+  map: map
 });
 
-var LayersShowAction = {
-  acidentes: function(){
-    sublayers[0].show();
-    return true;
-  },
-  acidentes_letais: function(){
-    sublayers[1].show();
-    return true;
-  }
-}
+var DEFAULT_CARTOCSS = [
+'#layer {',
+" marker-width: 3; ",
+' marker-fill: #FEE391; ',
+' [value > 2] { marker-fill: #FEC44F; }',
+' [value > 3] { marker-fill: #FE9929; }',
+' [value > 4] { marker-fill: #EC7014; }',
+' [value > 5] { marker-fill: #CC4C02; }',
+' [value > 6] { marker-fill: #993404; }',
+' [value > 7] { marker-fill: #662506; }',
+' [frame-offset = 1] { marker-width: 8;marker-fill-opacity: 0.05;}',
+' [frame-offset = 2] { marker-width: 20;marker-fill-opacity: 0.02;}',
+//' [frame-offset = 1] { marker-width: 25;marker-fill-opacity: 0.01;}',
+'}'
+].join('\n');
 
-var LayersHideAction = {
-  acidentes: function(){
-    sublayers[0].hide();
-    return true;
-  },
-  acidentes_letais: function(){
-    sublayers[1].hide();
-    return true;
-  }
-}
+torqueLayer.setCartoCSS(DEFAULT_CARTOCSS);
+torqueLayer.setMap(map);
+torqueLayer.play()
