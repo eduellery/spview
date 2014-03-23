fe9e7c8ae35182695d13cb7e947490b37e3e024c
eltermann <felipe.eltermann@gmail.com>
2014-03-23 07:19:32 -0300

    Adicionando retorno no waze_django

--- waze_django/waze_django/views.py
+++ waze_django/waze_django/views.py
@@ -6,12 +6,25 @@ class WazeEndpointHandler(APIView):
   renderer_classes = (renderers.UnicodeJSONRenderer, renderers.JSONRenderer)
 
   def get(self, request, *args, **kwargs):
-    response = {}
+    response = []
 
-    if 'x' in request.GET and 'y' in request.GET and 'z' in request.GET:
-      x = request.GET['x'][0]
-      y = request.GET['y'][0]
-      z = request.GET['z'][0]
+    if 'time_from' in request.GET and 'time_to' in request.GET:
+      # TODO - validate
+      time_from = request.GET['time_from']
+      time_to = request.GET['time_to']
+    else:
+      time_from = '2014-03-23 00'
+      time_to = '2014-03-25 00'
 
-    response = (x, y, z) # TODO
+    MONGO_HOST = 'localhost'
+    MONGO_PORT = 27017
+    MONGO_DB = 'waze'
+    MONGO_COLLECTION = 'jams'
+
+    conn = pymongo.Connection(MONGO_HOST, MONGO_PORT)
+    collection = conn[MONGO_DB][MONGO_COLLECTION]
+    # TODO - arrumar o formato do retorno
+    for doc in collection.find({'endtime': {'$gt': time_from, '$lt': time_to}}):
+      response.append(doc)
+
     return Response(response)
