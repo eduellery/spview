ee34752933685a3c0ccb8d6a74a9ab0862e7139e
Oscar Esgalha <oscaresgalha@gmail.com>
2014-03-23 01:03:24 -0300

    Heatmap de lentidao funcionando :metal:

--- web-app/app/assets/app.js
+++ web-app/app/assets/app.js
@@ -8,8 +8,7 @@ $('#camadas-button').click(function() {
 
 var map = new google.maps.Map(document.getElementById('map'), {
     center: new google.maps.LatLng(-23.546523, -46.633407),
-    zoom: 2,
-    mapTypeId:google.maps.MapTypeId.SATELLITE,
+    zoom: 10,
     mapTypeControl:false,
     minZoom:1,
     scaleControl: false,
@@ -18,32 +17,15 @@ var map = new google.maps.Map(document.getElementById('map'), {
 });
 
 var map_style = {};
-map_style.google_maps_customization_style = [
-    {
-        stylers:[
-            { invert_lightness: true },
-            { weight: 1 },
-            { saturation: -100 },
-            { lightness: -40 }
-        ]
-    },
-    {
-        elementType:"labels",
-        stylers:[
-            { visibility:"simplified" }
-        ]
-    }
-];
-
-map.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+map_style.google_maps_customization_style = [ { "featureType": "landscape", "stylers": [ { "visibility": "off" } ] },{ "stylers": [ { "lightness": -16 }, { "saturation": 1 }, { "gamma": 0.84 } ] } ];
 map.setOptions({styles:map_style.google_maps_customization_style});
 
 var torqueLayer = new torque.GMapsTorqueLayer({
   provider: 'sql_api',
   user : 'ellery',
-  table : 'lentidao_sumario_semanal3',
-  column : 'cartodb_id',
-  countby : 'count(extensao)',
+  table : 'lentidao_sumario_semanal6',
+  column : 'timestr',
+  countby : 'max(extensao)',
   resolution: 1,
   steps: 1024,
   blendmode : 'lighter',
@@ -53,17 +35,22 @@ var torqueLayer = new torque.GMapsTorqueLayer({
 
 var DEFAULT_CARTOCSS = [
 '#layer {',
-" marker-width: 3; ",
-' marker-fill: #FEE391; ',
-' [value > 2] { marker-fill: #FEC44F; }',
-' [value > 3] { marker-fill: #FE9929; }',
-' [value > 4] { marker-fill: #EC7014; }',
-' [value > 5] { marker-fill: #CC4C02; }',
-' [value > 6] { marker-fill: #993404; }',
-' [value > 7] { marker-fill: #662506; }',
-' [frame-offset = 1] { marker-width: 8;marker-fill-opacity: 0.05;}',
-' [frame-offset = 2] { marker-width: 20;marker-fill-opacity: 0.02;}',
-//' [frame-offset = 1] { marker-width: 25;marker-fill-opacity: 0.01;}',
+" marker-width: 5; ",
+' marker-fill: ; #B10026;',
+' [value > 10] { marker-fill: #E31A1C; }',
+' [value > 50] { marker-fill: #FC4E2A; }',
+' [value > 100] { marker-fill: #FD8D3C; }',
+' [value > 250] { marker-fill: #FEB24C; }',
+' [value > 500] { marker-fill: #FED976; }',
+' [value > 1000] { marker-fill: #FFFFB2; }',
+' [frame-offset = 1] {  marker-width: 6; marker-fill-opacity: 0.9;}',
+' [frame-offset = 2] {  marker-width: 7; marker-fill-opacity: 0.8;}',
+' [frame-offset = 3] {  marker-width: 8; marker-fill-opacity: 0.7;}',
+' [frame-offset = 4] {  marker-width: 9; marker-fill-opacity: 0.6;}',
+' [frame-offset = 5] {  marker-width: 10; marker-fill-opacity: 0.5;}',
+' [frame-offset = 6] {  marker-width: 11; marker-fill-opacity: 0.4;}',
+' [frame-offset = 7] {  marker-width: 12; marker-fill-opacity: 0.3;}',
+' [frame-offset = 8] {  marker-width: 13; marker-fill-opacity: 0.2;}',
 '}'
 ].join('\n');
 
